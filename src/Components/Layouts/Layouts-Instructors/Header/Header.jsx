154fa1a4916ac8b939329eae45f71d28d2ee194e
JuanitoZaZa <juanestebangonzalezcaro567@gmail.com>
2024-10-21 15:04:41 -0500

    Se crea el header de la pagina instructures y de redirecciona desde el home

--- src/Components/Layouts/Layouts-Instructors/Header/Header.jsx
+++ src/Components/Layouts/Layouts-Instructors/Header/Header.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Logo } from './../../../UI/UI-Instructor/Logo/Logo'
+import { Title } from './../../../UI/UI-Instructor/Title/Title'
+import { HomeIcon } from './../../../UI/UI-Instructor/Home-Icon/Home-Icon'
+import './Header.css'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <Logo />
+      <Title />
+      <HomeIcon />
+    </header>
   )
 }
--- src/Components/Pages/App/App.jsx
+++ src/Components/Pages/App/App.jsx
@@ -1,11 +1,12 @@
 import { useRoutes, BrowserRouter } from 'react-router-dom'
 import Home from '../Home/Home'
 import './App.css'
+import { Instructors } from '../Instructor/Instructor';
 
 const AppRoutes = () => {
     let routes = useRoutes([  
       { path: '/', element: <Home /> },
-    
+      { path: '/instructors' , element: <Instructors/>}
     ]);
   
     return routes;
